00e5b620b637e38d1cb7075a8e8659b1189beaaa
coolcoconut45 <googlleeissstinky@gmail.com>
2025-08-27 12:16:02 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+var aname
+var pass
+function nameEverything() {
+    aname = document.getElementById("myName").value;
+    pass = document.getElementById("myPass").value;
+    console.log(aname + " " + pass)
+    window.location.href='main.html'
+}
+function doMath() {
+    /* ew!
+    DisGusTing!
+    I hate this
+    please save me
+    oh god.
+    help. */
+    window.location.href = 'math.html';
+}
+
+function game() {
+    window.location.href = 'thing.html';
+}
+
+function goMogus() {
+    window.location.href = 'login.html';
+}
---
+++ math.js
@@ -0,0 +1,49 @@
+var numb1
+var numb2
+// get the random numbers
+function getRand() {
+    numb1 = Math.floor(Math.random() * 11);
+    numb2 = Math.floor(Math.random() * 11);
+    console.log(numb1 + " + " + numb2)
+    ayay = 0 + numb1 + numb2
+    console.log(ayay)
+}
+function showRand() {
+    var string2 = "<h2>" + numb1 + " + " + numb2 + " " + "</h2>";
+    document.getElementById("texter").innerHTML += string2;
+    thing = numb1 + numb2;
+    console.log(thing);
+    console.log("if you found this, good job!");
+}
+function getBoth() {
+    getRand();
+    showRand();
+}
+function checkAnswer() {
+    var jsi = false
+    possibleAnswer = document.getElementById("answerId").value
+    console.log(possibleAnswer)
+    number2 = parseInt(possibleAnswer)
+    if (possibleAnswer === '') {
+        var sure = confirm("No input, do you want to submit?")
+        if (sure == true) {
+            window.location.href='incorrect.html'
+        }
+        else {
+            jsi = true
+        }
+    }
+    if(number2 === numb1 + numb2) {
+        if (jsi === false) {
+            window.location.href='correct.html'
+        }
+    }
+    else {
+        if (jsi === false) {
+            window.location.href='incorrect.html'
+        }
+    }
+}
+function nextQues() {
+    window.location.href = 'math.html'
+}
